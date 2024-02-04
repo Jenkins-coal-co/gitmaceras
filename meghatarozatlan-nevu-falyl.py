e2a825359777aba090638ed8c39b6b1e99149cb6
Jenkins-coal-co <27c.kiss.kornel@bdeg.hu>
2024-02-04 20:19:58 +0100

    megcsinaltam a programot elemejo

---
+++ meghatarozatlan-nevu-falyl.py
@@ -0,0 +1,16 @@
+
+with open("astronauts.csv") as asztronotakfalyl:
+    asztronotak = asztronotakfalyl.readlines()
+honapok = []
+for line in asztronotak:
+    honapok.append(line.split(",")[4].split("/")[0])
+x, szuletesek = 1, []
+del honapok[0]
+while x != 13:
+    y = [honapok.count(str(x)), x, round(honapok.count(str(x)) / len(honapok) * 100, 1)]
+    szuletesek.append(y)
+    x += 1
+print("százalék  hónap_száma")
+print(sorted(szuletesek)[-1][2], sorted(szuletesek)[-1][1])
+print(sorted(szuletesek)[-2][2], sorted(szuletesek)[-2][1])
+print(sorted(szuletesek)[-3][2], sorted(szuletesek)[-3][1])
