294a679d03b1cd9429abbf5a69837a046492524b
Jenkins-coal-co <27c.kiss.kornel@bdeg.hu>
2025-02-01 14:27:23 +0100

    kettészedtem kalkulálást és nyomtatást

--- meghatarozatlan-nevu-falyl.py
+++ meghatarozatlan-nevu-falyl.py
@@ -5,15 +5,20 @@ def main():
     for line in asztronotak:
         honapok.append(line.split(",")[4].split("/")[0])
     del honapok[0]
-    kalkulalosdi_es_nyomtatosdi(honapok)
+    szuletesek = kalkulalosdi(honapok)
+    nyomtatosdi(szuletesek)
 
 
-def kalkulalosdi_es_nyomtatosdi(honapok):
+def kalkulalosdi(honapok):
     x, szuletesek = 1, []
     while x != 13:
         y = [honapok.count(str(x)), x, round(honapok.count(str(x)) / len(honapok) * 100, 1)]
         szuletesek.append(y)
         x += 1
+    return szuletesek
+
+
+def nyomtatosdi(szuletesek):
     print("százalék  hónap_száma")
     print(sorted(szuletesek)[-1][2], sorted(szuletesek)[-1][1])
     print(sorted(szuletesek)[-2][2], sorted(szuletesek)[-2][1])
